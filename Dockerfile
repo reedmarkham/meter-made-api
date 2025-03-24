48bd0c321e62a7b6879611e638445fa9d24f9248
Reed Markham <reedmarkham@gmail.com>
2025-03-24 13:37:06 -0400

    FastAPI to serve local model.pkl

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9
+
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+
+ENV APP_HOME=/root
+WORKDIR $APP_HOME
+COPY /src $APP_HOME/src
+
+CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ src/main.py
@@ -0,0 +1,36 @@
+import pickle
+
+import pandas as pd
+import numpy as np
+from pydantic import BaseModel
+import uvicorn
+from fastapi import FastAPI
+import lightgbm as lgb
+
+app = FastAPI()
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+class ParkingSpot(BaseModel):
+    d: str
+    h: int
+    x: float
+    y: float
+
+@app.post("/predict")
+def predict(data: ParkingSpot):
+
+    to_predict = data.dict()
+    to_predict['d'] = pd.to_datetime(to_predict['d']).toordinal()
+    to_predict['c'] = pd.to_datetime(to_predict['d']).date().day
+    to_predict['w'] = pd.to_datetime(to_predict['d']).date().isoweekday()
+    to_predict = {k: to_predict[k] for k in ['d', 'c', 'w', 'h', 'x', 'y']}
+    to_predict = pd.DataFrame(to_predict)
+
+    prediction = model.predict_proba(to_predict)
+
+    return {
+        "probabilities": prediction[0], 
+        "get_ticket": np.argmax(prediction[0])
+        }
