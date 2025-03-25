bac3f3f3a366e31e1f12bad00d89cbfcc7d70774
Reed Markham <reedmarkham@gmail.com>
2025-03-25 01:46:45 -0400

    fix CORS issue

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ import numpy as np
 from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
-import lightgbm as lgb
+from fastapi.middleware.cors import CORSMiddleware
+import lightgbm
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
